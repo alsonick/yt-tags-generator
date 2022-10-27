7e79a77751c38d89c0263c4f28fce8d3b3487d06
Nicholas <contactnicholas81@gmail.com>
2022-10-27 18:56:08 +0100

    fix?

--- parts/Navbar.tsx
+++ parts/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export const Navbar = () => {
   return (
     <nav
-      className="flex items-center justify-between w-full px-14 top-0 h-20"
+      className="flex items-center fixed justify-between w-full px-14 top-0 h-20"
       style={{ height: "5rem", paddingLeft: "3.5rem", paddingRight: "3.5rem" }}
     >
       <h1 className="font-bold text-xl tracking-tight text-black">
